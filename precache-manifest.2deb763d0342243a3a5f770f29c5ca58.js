e0c34dae4c6d2db81c6284eb6d72b74c6c694189
Glissando <justinjoelward@gmail.com>
2020-03-27 14:31:13 -0500

    Updates

---
+++ precache-manifest.2deb763d0342243a3a5f770f29c5ca58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70ec6e40d9d92ffa2382b16ab38ff44",
+    "url": "/trading-block/index.html"
+  },
+  {
+    "revision": "cc600f41c30fba0d988a",
+    "url": "/trading-block/static/css/main.acacee7f.chunk.css"
+  },
+  {
+    "revision": "61fad6096c77d6523386",
+    "url": "/trading-block/static/js/2.94f52ef3.chunk.js"
+  },
+  {
+    "revision": "c1267424a4914235c5861c32b01cd1fa",
+    "url": "/trading-block/static/js/2.94f52ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc600f41c30fba0d988a",
+    "url": "/trading-block/static/js/main.5726ff21.chunk.js"
+  },
+  {
+    "revision": "f3087254b40ea5524b5b",
+    "url": "/trading-block/static/js/runtime-main.157f3061.js"
+  }
+]);
